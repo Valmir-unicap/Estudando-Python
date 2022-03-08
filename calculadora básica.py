7de49a9924eaa241e268703060c8665bf6128303
Valmir Júnior <61609240+Valmir-unicap@users.noreply.github.com>
2022-03-07 22:12:24 -0300

    Update calculadora básica.py

--- calculadora básica.py
+++ calculadora básica.py
@@ -2,12 +2,14 @@
 print("Calculadora Básica")
 numero1= int(input("Digite o primeiro número: "))
 numero2= int(input("Digite o segundo número: ")) 
+print("Menu")
 print("1- Adição")
 print("2- Subtração")
 print("3- Divisão")
 print("4- Multiplicação")
+print("0- Sair do programa!")
 escolha= int(input("Escolha uma das opções acima: "))
-while escolha >= 5 or escolha <=0: #validacão da entrada de dados 
+while escolha >= 5 or escolha <0: #validacão da entrada de dados 
     print("Opção inválida")
     escolha= int(input("Escolha uma das opções acima: "))
 if escolha == 1:
@@ -19,6 +21,8 @@ elif escolha == 2:
 elif escolha == 3:
     divisao= numero1/numero2
     print("Resultado= ",divisao)
-else:
+elif escolha == 4:
     multiplica= numero1 * numero2
     print("Resultado= ",multiplica)
+else:
+    print("Fim do programa! @Developer Valmir Jr")
