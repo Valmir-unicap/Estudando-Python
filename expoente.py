4a78f871872d2a01616cf5ed1e0090c50748ae0b
Valmir Júnior <61609240+Valmir-unicap@users.noreply.github.com>
2022-04-06 23:12:51 -0300

    Create expoente.py

---
+++ expoente.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+print("Expoente")
+numero1= int(input("digite um número: "))
+elevado= int(input("digite o número elevado: ")) 
+expoente= numero1 ** elevado
+print("resultado = ",expoente)
